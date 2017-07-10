348df70f2126223c498074b4536e0cc0adb61450
MDreyar <stoeremitch@hotmail.com>
2017-07-10 22:32:49 +0200

    add comments to slave

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,13 +1,13 @@
 #include <Wire.h>
 
-#define A 3// For displaying segment "A"
-#define B 2 // For displaying segment "B"
-#define C 7 // For displaying segment "C"
-#define D 8 // For displaying segment "D"
-#define E 9 // For displaying segment "E"
-#define F 4 // For displaying segment "F"
-#define G 5 // For displaying segment "G"
-#define DP 6 // For displaying segment "."
+#define A 3   // For displaying segment "A"
+#define B 2   // For displaying segment "B"
+#define C 7   // For displaying segment "C"
+#define D 8   // For displaying segment "D"
+#define E 9   // For displaying segment "E"
+#define F 4   // For displaying segment "F"
+#define G 5   // For displaying segment "G"
+#define DP 6  // For displaying segment "."
 
 // Set debug mode with Serial
 const boolean DEBUG_MODE = true;
@@ -39,7 +39,7 @@ byte sendingData[3];
 
 void setup() {
   if (DEBUG_MODE) {
-    Serial.begin(9600);
+    Serial.begin(9600);                     // Begin serial communication
   }
 
   // For led display segments
@@ -67,76 +67,72 @@ void setup() {
   pinMode(liftArrived, INPUT);
 
   // For initialization
-  int address = determineAddress();
+  int address = determineAddress();         // Run initialization to determine the location of the segment
   Serial.print("address: ");
   Serial.println(address);
 
   // For communication
-  Wire.begin(address + 8); // https://www.arduino.cc/en/Reference/Wire inside NOTE: addresses should start from 8 
-  Wire.onReceive(getLiftRelatedData);
-  Wire.onRequest(sendDataToMaster);
+  Wire.begin(address + 8);                  // Start I2C communication // https://www.arduino.cc/en/Reference/Wire inside NOTE: addresses should start from 8 
+  Wire.onReceive(getLiftRelatedData);       // Set the event handler to handle data incoming from the master
+  Wire.onRequest(sendDataToMaster);         // Set the event handler to hande data sending to the master
 }
 
 void loop() {
-  keepReadingButtonUpAndDownUntilPressed(); // constantly read BUTTON_UP and DOWN until pressed
-  setSendingDataArray();                    // fill sending data array, so that wire function is clean!!!
-  handleDoorAndResetFloorButtons();         // turn led on or off if openDoor = true
-  // also reset lift buttons, because lift already arrived
-  ledDisplayHandler(liftPosition);
-  handleButtonLed();
+  keepReadingButtonUpAndDownUntilPressed(); // Constantly read BUTTON_UP and DOWN until pressed
+  setSendingDataArray();                    // Fill sending data array, so that wire function is clean
+  handleDoorAndResetFloorButtons();         // Turn led on or off and reset buttons
+  ledDisplayHandler(liftPosition);          // Set led display to the current floor of the lift
+  handleButtonLed();                        // Turn the button leds on and off
 }
 
 /*********************** I2C CODE ***********************/
 
-// get liftPosition and show it on LED display
-// NOTE: Dont add Serial.prints or complex code in I2C functions. MITCH PLZ ADD SOURCE :)
+// NOTE: Dont add Serial.prints or complex code in I2C functions.
 void getLiftRelatedData(int numBytes) {
-  liftPosition = Wire.read();           // receive bit for lift as an integer
-  openDoor = Wire.read();               // receive bit for floor door
-
-  //  Serial.println(liftPosition);  // remove after testing
-  // Serial.print("If openDoor = 1 LED ON + Reset floor button");
-  //  Serial.println(openDoor);  // remove after testing
+  // Get liftPosition and show it on LED display
+  liftPosition = Wire.read();           // Receive byte containing current lift position
+  openDoor = Wire.read();               // Receive byte containing whether the door should open
 }
 
-// send goingUpButton, goingDownButton and liftArrived (ir data)
 void sendDataToMaster() {
+  // Send goingUpButton, goingDownButton and liftArrived (ir data)
   Wire.write(sendingData, 3);
 }
 
 /*********************** INITIALIZE FLOOR CODE ***********************/
 
 int determineAddress() {
-  int selectingFloor = 0;
-  ledDisplayHandler(selectingFloor);
-  digitalWrite(DP, HIGH);
+  // Initializes the segment by determinging the location of the segment
+  int selectingFloor = 0;                                            // Set current selected location on 0
+  ledDisplayHandler(selectingFloor);                                 // Show the currenly selected location
+  digitalWrite(DP, HIGH);                                            // Show a dot on the led display to indicate initialization
   while (true) {
     if (DEBUG_MODE) {
       Serial.print("selectedFloor: ");
       Serial.println(selectingFloor);
     }
-    keepReadingButtonUpAndDownUntilPressed();
-    if (goingUpButtonPressed) {
+    keepReadingButtonUpAndDownUntilPressed();                        // Read the buttons on the segment
+    if (goingUpButtonPressed) {                                      // If the floor up button is pressed
       if (DEBUG_MODE) {
         Serial.println("goingUpButtonPressed");
       }
-      selectingFloor++;
-      goingUpButtonPressed = 0;
-      if (selectingFloor > 9) {
-        selectingFloor = 0;
+      selectingFloor++;                                              // Increase the currently selected floor by one
+      goingUpButtonPressed = 0;                                      // Reset the floor up button
+      if (selectingFloor > 9) {                                      // If the currently selected floor is more then 9
+        selectingFloor = 0;                                          // Reset the currently selected floor to 0
       }
-      ledDisplayHandler(selectingFloor);
-      digitalWrite(DP, HIGH);
+      ledDisplayHandler(selectingFloor);                             // Show the currently selected location
+      digitalWrite(DP, HIGH);                                        // Show a dot on the led display to indicate initialization
     } 
-    else if (goingDownButtonPressed) {
+    else if (goingDownButtonPressed) {                               // If the floor down button is pressed
       if (DEBUG_MODE) {
         Serial.println("Floor selected (goingDownButtonPressed)");
       }
-      turnOff();
-      delay(2000);
-      goingDownButtonPressed = 0;
-      digitalWrite(DP, LOW);
-      return selectingFloor;
+      turnOff();                                                     // Turn off led display to show successful initialization
+      delay(2000);                                                   // Wait 2 seconds
+      goingDownButtonPressed = 0;                                    // Reset the floor down button
+      digitalWrite(DP, LOW);                                         // Disable the dot to indicate normal function
+      return selectingFloor;                                         // Return the selected floor
     }
     delay(1000);
   }
@@ -145,8 +141,7 @@ int determineAddress() {
 /*********************** FLOOR CODE ***********************/
 
 void setSendingDataArray() {
-  //  Serial.println("Request received from master");
-  // Serial.println("Checking button state" );
+  // Fills the array with data to be send to the master
   if (DEBUG_MODE) {
     Serial.println("set SendingDataArray()");
   }
@@ -154,78 +149,72 @@ void setSendingDataArray() {
   checkLiftArrived();
 }
 
-// Send that I pressed the request lift button
-void sendButtonStatesGoingUpAndDown() {
-  // Serial.println(goingUpButtonPressed);   // remove after testing
-  // Serial.println(goingDownButtonPressed); // remove after testing
 
-  sendingData[0] = goingUpButtonPressed;
-  sendingData[1] = goingDownButtonPressed; 
+void sendButtonStatesGoingUpAndDown() {
+  // Fills the array with the current button states
+  sendingData[0] = goingUpButtonPressed;      // Puts button up state in the array
+  sendingData[1] = goingDownButtonPressed;    // Puts button down state in the array
 }
 
 void checkLiftArrived() {
+  // Checks to see if the IR-module is triggered and put it in the array
   liftArrived = digitalRead(IR_PIN);
 
-   //Serial.print("liftArrived: ");
-   //Serial.println(liftArrived);
+  liftArrived = !liftArrived;       // Invert output because the IR-module returns false if it is triggered
 
-  liftArrived = !liftArrived; // because output is false if object arrived
-
-    sendingData[2] = liftArrived;
+    sendingData[2] = liftArrived;   // Put it in the array
 }
 
 void keepReadingButtonUpAndDownUntilPressed() {
-  if (!goingUpButtonPressed) {
-    goingUpButtonPressed = digitalRead(BUTTON_UP_PIN);
+  // Reads the buttons until the are pressed
+  if (!goingUpButtonPressed) {                              // If button up is not pressed
+    goingUpButtonPressed = digitalRead(BUTTON_UP_PIN);      // Read button up
   }
-  if (!goingDownButtonPressed) {
-    goingDownButtonPressed = digitalRead(BUTTON_DOWN_PIN);
+  if (!goingDownButtonPressed) {                            // If button down is not pressed
+    goingDownButtonPressed = digitalRead(BUTTON_DOWN_PIN);  // Read button down
   }
-
-  // Serial.print("goingUpButtonPressed: ");
-  // Serial.println(goingUpButtonPressed);
-  // Serial.print("goingDownButtonPressed: ");
-  // Serial.println(goingDownButtonPressed);
 }
 
 void handleDoorAndResetFloorButtons() {
-  if (openDoor) {
-    resetFloorButtons(); 
+  // Turns on the door led if the doors are open en resets the buttons
+  if (openDoor) {                       // If door should be open
+    resetFloorButtons();                // Reset buttons
 
-    digitalWrite(DOOR_LED_PIN, HIGH);
+    digitalWrite(DOOR_LED_PIN, HIGH);   // Turn on door led
   } 
-  else {
-    digitalWrite(DOOR_LED_PIN, LOW);
+  else {                                // If door should be closed
+    digitalWrite(DOOR_LED_PIN, LOW);    // Turn off door led
   }
-  // Serial.print("openDoor: ");
-  // Serial.println(openDoor);
 }
 
 void resetFloorButtons() {
+  // Resets the buttons
   if (DEBUG_MODE) {
     Serial.println("floor buttons reset");
   }
-  goingUpButtonPressed = 0;
-  goingDownButtonPressed = 0;
+  goingUpButtonPressed = 0;     // Turn button up off
+  goingDownButtonPressed = 0;   // Turn button down off
 }
 
 void ledDisplayHandler(int x) {
-  if (x >= 0 && x <= 9) {
-    turnOff();
-    displayDigit(x); 
+  // Shows a number on the led display
+  if (x >= 0 && x <= 9) {   // If the number is between 0-9
+    turnOff();              // Turn off the led display
+    displayDigit(x);        // Show the number on the led display
   }
 }
 
 void handleButtonLed(){
-  if (goingUpButtonPressed) {
-    digitalWrite(BUTTON_UP_LED_PIN, HIGH);
-  } else {
-    digitalWrite(BUTTON_UP_LED_PIN, LOW);
+  // Turn the button leds on and off
+  if (goingUpButtonPressed) {                     // If button up is on
+    digitalWrite(BUTTON_UP_LED_PIN, HIGH);        // Turn button up led on
+  } else {                                        // If button up is off
+    digitalWrite(BUTTON_UP_LED_PIN, LOW);         // Turn button up led off
   }
-  if (goingDownButtonPressed) {
-    digitalWrite(BUTTON_DOWN_LED_PIN, HIGH);
-  } else {
-    digitalWrite(BUTTON_DOWN_LED_PIN, LOW);
+  if (goingDownButtonPressed) {                   // If button down is on
+    digitalWrite(BUTTON_DOWN_LED_PIN, HIGH);      // Turn button down led on
+  } else {                                        // If button down is off
+    digitalWrite(BUTTON_DOWN_LED_PIN, LOW);       // Turn button down led off
   }
 }
 
@@ -233,6 +222,7 @@ void handleButtonLed(){
 // zijn aan de led segmenten:
 // https://cdn.instructables.com/ORIG/FA0/S3TG/GZUAG6G2/FA0S3TGGZUAG6G2.gif
 void displayDigit(int digit) {
+  // Handles showing a specific digit on the led display
   // Conditions for displaying segment A
   if(digit != 1 && digit != 4) {
     digitalWrite(A, HIGH);
@@ -264,6 +254,7 @@ void displayDigit(int digit) {
 }
 
 void turnOff() {
+  // Turns off the led display
   digitalWrite(A, LOW);
   digitalWrite(B, LOW);
   digitalWrite(C, LOW);
